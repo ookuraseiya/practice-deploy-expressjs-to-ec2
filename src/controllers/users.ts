a520f3f95026ce8c1c33e9743c433370cacb2aee
ookuraseiya <seiya.ookura@gmail.com>
2026-04-13 20:36:12 +0900

    fix

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,47 +5,35 @@ dotenv.config();
 
 const router: Router = express.Router();
 
-const connection = mysql.createConnection({
+const dbConfig = {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: Number(process.env.MYSQL_PORT) || 3306, // ポートは数値型にする
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('MySQL接続エラー詳細:', err);
-    return;
-  }
-  console.log(`MySQL接続成功! (Host: ${process.env.MYSQL_HOST})`);
-});
+  port: Number(process.env.MYSQL_PORT) || 3306,
+};
 
 router.get('/', (_req: Request, res: Response) => {
-  connection.query('SELECT * FROM todo', (err, results) => {
+  const connection = mysql.createConnection(dbConfig);
+  connection.connect((err) => {
     if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+      console.error('MySQL接続エラー詳細:', err);
+      return res.status(500).json({
+        message:
+          'データベース接続に失敗しました。/users 以外のルートは生きています。',
+        error: err.message,
+      });
     }
-    res.send(results);
-  });
-});
 
-// データ追加
-router.post('/', (req: Request, res: Response) => {
-  const { id, todo } = req.body;
-
-  connection.query(
-    'INSERT INTO todo (id, todo) VALUES (?, ?)',
-    [id, todo],
-    (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send(err);
+    connection.query('SELECT * FROM todo', (queryErr, results) => {
+      connection.end();
+      if (queryErr) {
+        console.error(queryErr);
+        return res.status(500).send(queryErr);
       }
-      res.send({ message: '追加成功', results });
-    }
-  );
+      res.send(results);
+    });
+  });
 });
 
 export default router;
